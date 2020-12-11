0662b3cbdeee7f83ee68fe7b64950b5c27d8c3bf
unknown <luisfelipesdn12@gmail.com>
2020-12-11 10:11:25 -0300

    Added tests for errors

--- test.js
+++ test.js
@@ -10,3 +10,39 @@ test("main", t => {
 		minimum: 1
 	})
 })
+
+test("Expected an array of numbers error", t => {
+	// Each test case is an array to be unpacked
+	// and used as a list of arguments
+	const testCases = [
+		[42],
+		[-15, 1, 2, 4, 3, 5, 8, 6, 7, 9, 10, 25],
+		[{ values: [-15, 1, 2, 4, 3, 5, 8, 6, 7, 9, 10, 25] }]
+	]
+
+	t.plan(testCases.length * 2)
+
+	for (const testCase of testCases) {
+		const error = t.throws(() => boxPlot(...testCase))
+
+		t.is(error.message, "Expected an array of numbers")
+	}
+})
+
+test("Expected a number error", t => {
+	// Each test case is an array to
+	// be used as the only argument
+	const testCases = [
+		["42", "-15", 1, 2],
+		["-15", "1", "2", "4"],
+		[{ value: -15 }, { value: 1 }, { value: 2 }, { value: 4 }]
+	]
+
+	t.plan(testCases.length * 2)
+
+	for (const testCase of testCases) {
+		const error = t.throws(() => boxPlot(testCase))
+
+		t.is(error.message, "Expected a number")
+	}
+})
