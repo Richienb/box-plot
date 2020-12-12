e1fb5f374788fb04ed539aeaf7d7758d5e07c938
Luis Felipe Santos do Nascimento <luisfelipesdn12@gmail.com>
2020-12-11 23:45:20 -0300

    Test error handling (#1)

--- test.js
+++ test.js
@@ -10,3 +10,27 @@ test("main", t => {
 		minimum: 1
 	})
 })
+
+test("Expected an array of numbers error", t => {
+	const testCases = [
+		[42],
+		[-15, 1, 2, 4, 3, 5, 8, 6, 7, 9, 10, 25],
+		[{ values: [-15, 1, 2, 4, 3, 5, 8, 6, 7, 9, 10, 25] }]
+	]
+
+	for (const testCase of testCases) {
+		t.throws(() => boxPlot(...testCase), { message: "Expected an array of numbers" })
+	}
+})
+
+test("Expected a number error", t => {
+	const testCases = [
+		["42", "-15", 1, 2],
+		["-15", "1", "2", "4"],
+		[{ value: -15 }, { value: 1 }, { value: 2 }, { value: 4 }]
+	]
+
+	for (const testCase of testCases) {
+		t.throws(() => boxPlot(testCase), { message: "Expected a number" })
+	}
+})
